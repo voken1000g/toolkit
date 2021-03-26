ce6cc9a6b49e3cde011aae3ee474fdf40892aba3
Lyman Snow <pmvoken@protonmail.com>
2021-03-26 16:30:57 +0000

    updated: home.js

--- utils/constants/home.js
+++ utils/constants/home.js
@@ -4,67 +4,42 @@ const HOST = location.host.split(':')[0]
 
 console.log('::: home.js HOST:', HOST)
 
-const HOMES = {
-  'default': {
-    title: 'VOKEN_Toolkit',
-    path: '/',
-    icon: ['fas', 'tools'],
-  },
-
-  'localhost': {
-    title: 'VOKEN_Toolkit',
-    path: '/voken/early-bird',
-    icon: ['fas', 'tools'],
-  },
-
-  // Toolkit
-  'toolkit': {
-    title: 'VOKEN_Toolkit',
-    path: '/wallet',
-    icon: ['fas', 'tools'],
-  },
-  'toolkit.voken.io': {
-    title: 'VOKEN_Toolkit',
-    path: '/wallet',
-    icon: ['fas', 'tools'],
-  },
-
-  // EarlyBird
-  'early-bird': {
-    title: 'VOKEN_Early_Bird_Sale',
-    path: '/voken/early-bird',
-    icon: ['fas', 'gifts'],
-  },
-  'get.voken.io': {
-    title: 'VOKEN_Early_Bird_Sale',
-    path: '/voken/early-bird',
-    icon: ['fas', 'gifts'],
-  },
-  'early-bird.voken.io': {
-    title: 'VOKEN_Early_Bird_Sale',
-    path: '/voken/early-bird',
-    icon: ['fas', 'gifts'],
-  },
-
-  // Migrate
-  'migrate': {
-    title: 'VOKEN_Toolkit',
-    path: '/voken/migrate',
-    icon: ['fas', 'tools'],
-  },
-  'migrate.voken.io': {
-    title: 'VOKEN_Toolkit',
-    path: '/voken/migrate',
-    icon: ['fas', 'tools'],
-  },
-}
-
-const HOME = HOMES[HOST]
-
 export default function() {
-  if (repo) {
-    return HOMES[repo]
+  const key = repo ? repo : HOST
+  switch (key) {
+    case 'toolkit':
+    case 'toolkit.voken.io':
+      return {
+        title: 'VOKEN_Toolkit',
+        path: '/wallet',
+        icon: ['fas', 'tools'],
+      }
+    case 'early-bird':
+    case 'get.voken.io':
+    case 'early-bird.voken.io':
+      return {
+        title: 'VOKEN_Early_Bird_Sale',
+        path: '/voken/early-bird',
+        icon: ['fas', 'gifts'],
+      }
+    case 'migrate':
+    case 'migrate.voken.io':
+      return {
+        title: 'VOKEN_Toolkit',
+        path: '/voken/migrate',
+        icon: ['fas', 'tools'],
+      }
+    case 'localhost':
+      return {
+        title: 'VOKEN_Toolkit',
+        path: '/wallet',
+        icon: ['fas', 'tools'],
+      }
+    default:
+      return {
+        title: 'VOKEN_Toolkit',
+        path: '/',
+        icon: ['fas', 'tools'],
+      }
   }
-
-  return HOME ? HOME : HOMES['default']
 }
